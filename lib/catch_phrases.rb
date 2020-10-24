a93086664d08bc493144e4a8d867f2393d3a4b9a
Erwin Feng <efeng732@gmail.com>
2020-10-24 23:07:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
 
 
-def mario (phrase ="It's-a me, Mario!")
-  
+def mario
+  phrase ="It's-a me, Mario!"
   puts phrase
 end
 
@@ -9,3 +9,14 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  hyrule = "It's Dangerous To Go Alone! Take This."
+  puts hyrule
+end
+
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
+end 
